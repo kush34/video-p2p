56a0fdcb1aa0c69ebee66e755f7cb800f5de85d3
kush34 <chattkush34@gmail.com>
2025-05-18 15:46:54 +0530

    added env URL

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import 'dotenv/config'
 import {Server} from 'socket.io';
 import cors from 'cors';
 import http from 'http';
@@ -8,7 +9,7 @@ const app = express();
 const server  = http.createServer(app);
 const  io = new Server(server,{
 		cors: {
-		  origin: "http://localhost:5173",
+		  origin: process.env.Frontend_URL,
 		  methods: ["GET", "POST"]
 		}
 });
